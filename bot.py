f28943bb7fba0cfcfcda8441e2fafd3d0eb07798
Fawwaz Thoerif <fawwazthoerif@gmail.com>
2024-07-02 22:40:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,8 @@ class PixelTapTod:
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
         }
         self.marin_kitagawa = lambda data: {
-            key: value[0] for key, value in parse_qs(data).items()}
+            key: value[0] for key, value in parse_qs(data).items()
+        }
 
     def get_secret(self, userid):
         rawr = "adwawdasfajfklasjglrejnoierjboivrevioreboidwa"
@@ -50,178 +51,180 @@ class PixelTapTod:
 
     def data_parsing(self, data):
         redata = {}
-        for i in unquote(data).split('&'):
-            key, value = i.split('=')
+        for i in unquote(data).split("&"):
+            key, value = i.split("=")
             redata[key] = value
 
         return redata
 
     def get_me(self, data: Data):
-        url = 'https://api-clicker.pixelverse.xyz/api/users'
+        url = "https://api-clicker.pixelverse.xyz/api/users"
         headers = self.base_headers.copy()
-        headers['initData'] = data.init_data
-        headers['secret'] = data.secret
-        headers['tg-id'] = data.userid
+        headers["initData"] = data.init_data
+        headers["secret"] = data.secret
+        headers["tg-id"] = data.userid
         if data.username is not None:
-            headers['username'] = data.username
+            headers["username"] = data.username
 
         while True:
             res = self.http(url, headers)
-            balance = res.json().get('clicksCount', None)
+            balance = res.json().get("clicksCount", None)
             if balance is None:
-                self.log(f'{kuning}failed fetch balance !')
+                self.log(f"{kuning}failed fetch balance !")
                 time.sleep(2)
                 continue
-            self.log(f'{hijau}total balance : {putih}{balance}')
+            self.log(f"{hijau}total balance : {putih}{balance}")
             self.user_balance = balance
             return
 
     def daily_reward(self, data: Data):
-        url = 'https://api-clicker.pixelverse.xyz/api/daily-rewards'
+        url = "https://api-clicker.pixelverse.xyz/api/daily-rewards"
         headers = self.base_headers.copy()
-        headers['initData'] = data.init_data
-        headers['secret'] = data.secret
-        headers['tg-id'] = data.userid
+        headers["initData"] = data.init_data
+        headers["secret"] = data.secret
+        headers["tg-id"] = data.userid
         if data.username is not None:
-            headers['username'] = data.username
+            headers["username"] = data.username
 
         res = self.http(url, headers)
-        today_reward = res.json().get('todaysRewardAvailable')
+        today_reward = res.json().get("todaysRewardAvailable")
         if today_reward is None:
-            self.log(f'{merah}failed fetch today reward !')
+            self.log(f"{merah}failed fetch today reward !")
             return
 
         if today_reward:
-            url_claim = 'https://api-clicker.pixelverse.xyz/api/daily-rewards/claim'
-            res = self.http(url_claim, headers, '')
-            amount = res.json().get('amount')
+            url_claim = "https://api-clicker.pixelverse.xyz/api/daily-rewards/claim"
+            res = self.http(url_claim, headers, "")
+            amount = res.json().get("amount")
             if amount is None:
-                self.log(f'{merah}failed claim today reward !')
+                self.log(f"{merah}failed claim today reward !")
                 return
 
-            self.log(f'{hijau}success claim today reward : {putih}{amount}')
+            self.log(f"{hijau}success claim today reward : {putih}{amount}")
             return
 
-        self.log(f'{kuning}already claim today reward !')
+        self.log(f"{kuning}already claim today reward !")
         return
 
     def get_mining_proccess(self, data: Data):
         url = "https://api-clicker.pixelverse.xyz/api/mining/progress"
         headers = self.base_headers.copy()
-        headers['initData'] = data.init_data
-        headers['secret'] = data.secret
-        headers['tg-id'] = data.userid
+        headers["initData"] = data.init_data
+        headers["secret"] = data.secret
+        headers["tg-id"] = data.userid
         if data.username is not None:
-            headers['username'] = data.username
+            headers["username"] = data.username
 
         while True:
             res = self.http(url, headers)
-            if res.json().get('currentlyAvailable', None) is None:
+            if res.json().get("currentlyAvailable", None) is None:
                 continue
             break
-        available = res.json()['currentlyAvailable']
-        min_claim = res.json()['minAmountForClaim']
-        self.log(f'{putih}amount available : {hijau}{available}')
+        available = res.json()["currentlyAvailable"]
+        min_claim = res.json()["minAmountForClaim"]
+        self.log(f"{putih}amount available : {hijau}{available}")
         if available > min_claim:
-            url_claim = 'https://api-clicker.pixelverse.xyz/api/mining/claim'
-            res = self.http(url_claim, headers, '')
-            if 'claimedAmount' not in res.json().keys():
-                self.log(f'{merah}claim failed, maybe to many request !')
+            url_claim = "https://api-clicker.pixelverse.xyz/api/mining/claim"
+            res = self.http(url_claim, headers, "")
+            if "claimedAmount" not in res.json().keys():
+                self.log(f"{merah}claim failed, maybe to many request !")
                 return
 
-            claim_amount = res.json()['claimedAmount']
-            self.log(f'{hijau}claim amount : {putih}{claim_amount}')
+            claim_amount = res.json()["claimedAmount"]
+            self.log(f"{hijau}claim amount : {putih}{claim_amount}")
             return
 
-        self.log(f'{kuning}amount too small to make claim !')
+        self.log(f"{kuning}amount too small to make claim !")
         return
 
     def daily_combo(self, data: Data):
         url = "https://api-clicker.pixelverse.xyz/api/cypher-games/current"
         headers = self.base_headers.copy()
-        headers['initData'] = data.init_data
-        headers['secret'] = data.secret
-        headers['tg-id'] = data.userid
+        headers["initData"] = data.init_data
+        headers["secret"] = data.secret
+        headers["tg-id"] = data.userid
         if data.username is not None:
-            headers['username'] = data.username
+            headers["username"] = data.username
 
         res = self.http(url, headers)
         if res.status_code != 200:
-            self.log(f'{kuning}you have complete daily combo !')
+            self.log(f"{kuning}you have complete daily combo !")
             return
 
         today = datetime.now(timezone.utc).isoformat().split("T")[0]
-        list_combo = open("combo.txt").read().strip().split(',')
-        if len(list_combo) <= 0:
-            self.log(f'{kuning}skip apply daily combo, because combo.txt is empty !')
+        list_combo = open("combo.txt").read().strip().split(",")
+        if len(list_combo) <= 1:
+            self.log(f"{kuning}skip apply daily combo, because combo.txt is empty !")
             return
-        list_option = res.json().get('availableOptions')
+        list_option = res.json().get("availableOptions")
         if list_option is None:
-            self.log(f'{merah}failed fetch combo option !')
+            self.log(f"{merah}failed fetch combo option !")
             return
         list_option = [i["id"] for i in list_option]
         combo = {list_option[int(v) - 1]: k for k, v in enumerate(list_combo)}
-        combo_id = res.json()['id']
-        answer_url = f"https://api-clicker.pixelverse.xyz/api/cypher-games/{combo_id}/answer"
+        combo_id = res.json()["id"]
+        answer_url = (
+            f"https://api-clicker.pixelverse.xyz/api/cypher-games/{combo_id}/answer"
+        )
         res = requests.post(answer_url, headers=headers, json=combo)
         if res.status_code != 201:
-            self.log(f'{merah}failed apply daily combo !')
+            self.log(f"{merah}failed apply daily combo !")
             return
 
-        self.log(f'{hijau}success apply daily combo !')
-        reward = res.json().get('rewardAmount', None)
-        self.log(f'{hijau}reward daily combo : {putih}{reward}')
+        self.log(f"{hijau}success apply daily combo !")
+        reward = res.json().get("rewardAmount", None)
+        self.log(f"{hijau}reward daily combo : {putih}{reward}")
 
     def pets(self, data: Data):
         url = "https://api-clicker.pixelverse.xyz/api/pets"
         headers = self.base_headers.copy()
-        headers['initData'] = data.init_data
-        headers['secret'] = data.secret
-        headers['tg-id'] = data.userid
+        headers["initData"] = data.init_data
+        headers["secret"] = data.secret
+        headers["tg-id"] = data.userid
         if data.username is not None:
-            headers['username'] = data.username
+            headers["username"] = data.username
         while True:
             if self.AUTO_BUY_PET:
-                self.log(f'{putih}auto buy pet is {hijau}enable !')
+                self.log(f"{putih}auto buy pet is {hijau}enable !")
                 res = self.http(f"{url}/buy", headers, "")
-                _pet = res.json().get('id')
+                _pet = res.json().get("id")
                 if _pet is None:
-                    self.log(f'{merah}failed buy new pet !')
+                    self.log(f"{merah}failed buy new pet !")
                 else:
-                    _pet_name = res.json()['pet']['name']
+                    _pet_name = res.json()["pet"]["name"]
                     self.log(
-                        f'{hijau}success buy new pet, pet name : {putih}{_pet_name} !')
+                        f"{hijau}success buy new pet, pet name : {putih}{_pet_name} !"
+                    )
             res = self.http(url, headers)
-            data_pets = res.json().get('data', [])
+            data_pets = res.json().get("data", [])
             if len(data_pets) <= 0:
-                self.log(f'{merah}failed fetch data pets !')
+                self.log(f"{merah}failed fetch data pets !")
                 continue
 
             for pet in data_pets:
-                user_pet_name = pet['name']
-                user_pet_id = pet['userPet']['id']
-                user_pet_level = pet['userPet']['level']
-                user_level_up_price = pet['userPet']['levelUpPrice']
-                self.log(f'{hijau}pet id : {putih}{user_pet_id}')
-                self.log(f'{hijau}pet name : {putih}{user_pet_name}')
-                self.log(f'{hijau}pet level : {putih}{user_pet_level}')
+                user_pet_name = pet["name"]
+                user_pet_id = pet["userPet"]["id"]
+                user_pet_level = pet["userPet"]["level"]
+                user_level_up_price = pet["userPet"]["levelUpPrice"]
+                self.log(f"{hijau}pet id : {putih}{user_pet_id}")
+                self.log(f"{hijau}pet name : {putih}{user_pet_name}")
+                self.log(f"{hijau}pet level : {putih}{user_pet_level}")
                 if self.AUTO_UPGRADE_PET:
                     if user_pet_level > self.MAX_LEVEL_UPGRADE_PET:
-                        self.log(
-                            f'{kuning}limit max level pet reacted from config !')
+                        self.log(f"{kuning}limit max level pet reacted from config !")
                         continue
 
                     if user_level_up_price > self.user_balance:
                         self.log(
-                            f'{kuning}balance not enough to upgrade {putih}{user_pet_name}{kuning} !')
+                            f"{kuning}balance not enough to upgrade {putih}{user_pet_name}{kuning} !"
+                        )
                         continue
                     upgrade_url = f"https://api-clicker.pixelverse.xyz/api/pets/user-pets/{user_pet_id}/level-up"
-                    res = self.http(upgrade_url, headers, '')
+                    res = self.http(upgrade_url, headers, "")
                     if res.status_code != 201:
-                        self.log(f'{merah}failed upgrade pet {user_pet_name}')
+                        self.log(f"{merah}failed upgrade pet {user_pet_name}")
                         continue
-                    self.log(
-                        f'{hijau}success upgrade pet {putih}{user_pet_name}')
+                    self.log(f"{hijau}success upgrade pet {putih}{user_pet_name}")
                     self.user_balance -= user_level_up_price
                     continue
 
@@ -235,28 +238,28 @@ class PixelTapTod:
     {hijau}Github : {putih}@AkasakaID
         """
         arg = argparse.ArgumentParser()
-        arg.add_argument('--marin', action="store_true")
-        arg.add_argument('--data', default="data.txt", help="")
+        arg.add_argument("--marin", action="store_true")
+        arg.add_argument("--data", default="data.txt", help="")
         args = arg.parse_args()
         if args.marin is False:
             os.system("cls" if os.name == "nt" else "clear")
 
         print(banner)
-        print('~' * 50)
+        print("~" * 50)
         self.load_config()
         while True:
             for no, data in enumerate(self.load_data(args.data)):
-                self.log(f'{hijau}account number : {putih}{no + 1}')
+                self.log(f"{hijau}account number : {putih}{no + 1}")
                 parser = self.marin_kitagawa(data)
-                user = json.loads(parser['user'])
-                userid = str(user['id'])
-                first_name = user['first_name']
-                last_name = user['last_name']
+                user = json.loads(parser["user"])
+                userid = str(user["id"])
+                first_name = user["first_name"]
+                last_name = user["last_name"]
                 username = None
                 if "username" in user.keys():
-                    username = user['username']
+                    username = user["username"]
 
-                self.log(f'{hijau}login as : {putih}{first_name} {last_name}')
+                self.log(f"{hijau}login as : {putih}{first_name} {last_name}")
                 secret = self.get_secret(userid)
                 new_data = Data(data, userid, username, secret)
                 self.get_me(new_data)
@@ -264,18 +267,18 @@ class PixelTapTod:
                 self.daily_reward(new_data)
                 self.get_mining_proccess(new_data)
                 self.pets(new_data)
-                print('~' * 50)
+                print("~" * 50)
                 self.countdown(self.DEFAULT_INTERVAL)
             self.countdown(self.DEFAULT_COUNTDOWN)
 
     def load_config(self):
         config = json.loads(open("config.json").read())
-        self.DEFAULT_COUNTDOWN = config['countdown']
-        self.DEFAULT_INTERVAL = config['interval']
-        self.AUTO_BUY_PET = config['auto_buy_pet']
-        self.AUTO_UPGRADE_PET = config['auto_upgrade_pet']
-        self.MAX_LEVEL_UPGRADE_PET = config['max_level_upgrade_pet']
-        self.MAX_PET = config['max_pet']
+        self.DEFAULT_COUNTDOWN = config["countdown"]
+        self.DEFAULT_INTERVAL = config["interval"]
+        self.AUTO_BUY_PET = config["auto_buy_pet"]
+        self.AUTO_UPGRADE_PET = config["auto_upgrade_pet"]
+        self.MAX_LEVEL_UPGRADE_PET = config["max_level_upgrade_pet"]
+        self.MAX_PET = config["max_pet"]
 
     def load_data(self, file):
         if not os.path.exists(file):
@@ -285,7 +288,7 @@ class PixelTapTod:
         if len(datas) <= 0:
             self.log(f"{merah}there no account detected, fill {file} first !")
             sys.exit()
-        self.log(f'{hijau}account detected : {putih}{len(datas)}')
+        self.log(f"{hijau}account detected : {putih}{len(datas)}")
         return datas
 
     def countdown(self, t):
@@ -295,8 +298,7 @@ class PixelTapTod:
             jam = str(jam).zfill(2)
             menit = str(menit).zfill(2)
             detik = str(detik).zfill(2)
-            print(f"{putih}waiting until {jam}:{menit}:{detik} ",
-                  flush=True, end="\r")
+            print(f"{putih}waiting until {jam}:{menit}:{detik} ", flush=True, end="\r")
             t -= 1
             time.sleep(1)
         print("                          ", flush=True, end="\r")
@@ -310,23 +312,25 @@ class PixelTapTod:
             try:
                 if data is None:
                     res = requests.get(url, headers=headers, timeout=30)
-                    open('http.log', 'a',
-                         encoding='utf-8').write(f'{res.text}\n')
+                    open("http.log", "a", encoding="utf-8").write(f"{res.text}\n")
                     return res
 
-                if data == '':
+                if data == "":
                     res = requests.post(url, headers=headers, timeout=30)
-                    open('http.log', 'a',
-                         encoding='utf-8').write(f'{res.text}\n')
+                    open("http.log", "a", encoding="utf-8").write(f"{res.text}\n")
                     return res
 
-                res = requests.post(url, headers=headers,
-                                    data=data, timeout=30)
-                open('http.log', 'a', encoding='utf-8').write(f'{res.text}\n')
+                res = requests.post(url, headers=headers, data=data, timeout=30)
+                open("http.log", "a", encoding="utf-8").write(f"{res.text}\n")
                 return res
 
-            except (requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout, requests.exceptions.Timeout):
-                self.log(f'{merah}connection error / connection timeout !')
+            except (
+                requests.exceptions.ConnectionError,
+                requests.exceptions.ConnectTimeout,
+                requests.exceptions.ReadTimeout,
+                requests.exceptions.Timeout,
+            ):
+                self.log(f"{merah}connection error / connection timeout !")
                 continue
 
 
